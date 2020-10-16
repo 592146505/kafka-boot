e81f0c41afe135e7a0b0a1471f4cdc826dfecd26
roamer <592146505@qq.com>
2020-10-16 18:34:44 +0800

    Initial commit

---
+++ src/main/groovy/com/roamer/demo/kafka/boot/KafkaBootApplication.groovy
@@ -0,0 +1,13 @@
+package com.roamer.demo.kafka.boot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KafkaBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KafkaBootApplication, args)
+    }
+
+}
